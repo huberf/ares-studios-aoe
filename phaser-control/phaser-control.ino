e2fce2f74b33ae83a6486e2345a83278e97dc893
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-10-20 14:25:27 -0400

    Add all phaser upgrades fully compatible with current hardware

--- phaser-control/phaser-control.ino
+++ phaser-control/phaser-control.ino
@@ -4,8 +4,12 @@
 // Constants
 const int SHOT_CAPACITY = 6;
 const int TRIGGER_BUTTON = 5;
-const int LASER_PIN = 6
+const int LASER_PIN = 6;
 const int BUZZER_PIN = 9;
+
+// States
+boolean isFiring = false;
+boolean buttonPressedLastLoop = false;
 
 int timeFired = 0;
 int shotsLeft = SHOT_CAPACITY;
@@ -15,8 +19,10 @@ int timer = 0;
 int beforeTime = millis();
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Booting phaser...");
   pinMode(TRIGGER_BUTTON, INPUT);
-  pinMode(LASER_PIN, OUTPUT)
+  pinMode(LASER_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
 }
 
@@ -25,30 +31,47 @@ void loop() {
   beforeTime = millis();
   // put your main code here, to run repeatedly:
   if (isTriggerPulled()) {
-    fireGun();
+    if (isFiring == false && buttonPressedLastLoop == false) {
+      Serial.println("Firing shot");
+      fireGun();
+    }
+    buttonPressedLastLoop = true;
+  } else {
+    buttonPressedLastLoop = false;
   }
+  delay(10);
 }
 
 boolean isTriggerPulled() {
   boolean buttonPressed = digitalRead(TRIGGER_BUTTON);
-  return buttonPressed;
+  return buttonPressed == HIGH;
 }
 
 void fireGun() {
   if (shotsLeft > 0) {
+    isFiring = true;
+    //soundFX(3.0,100.0);
+    buzzPiezzo(2000, 100);
     digitalWrite(LASER_PIN, HIGH);
     int fireDuration = 100;
-    delay(fireDuration)
+    delay(fireDuration);
+    digitalWrite(LASER_PIN, LOW);
+    isFiring = false;
     timeFired += fireDuration;
     shotsLeft -= 1;
   } else {
-    alertLowAmmo()
+    alertLowAmmo();
   }
 }
 
 void alertLowAmmo() {
   // TODO: Write full logic to inform user of low ammo
-  buzzPiezzo(1000, 500)
+  for (int i = 0; i < 10; i++) {
+    buzzPiezzo(1000, 50);
+    delay(50);
+    buzzPiezzo(500, 50);
+    delay(50);
+  }
 }
 
 void updateDisplay() {
@@ -57,4 +80,15 @@ void updateDisplay() {
 
 void buzzPiezzo(int frequency, int duration) {
   tone(BUZZER_PIN, frequency, duration);
+}
+
+// Taken from: https://forum.arduino.cc/index.php?topic=118757.0
+void soundFX(float amplitude,float period){ 
+ int uDelay=2+amplitude+amplitude*sin(millis()/period);
+ for(int i=0;i<5;i++){
+   digitalWrite(BUZZER_PIN,HIGH);
+   delayMicroseconds(uDelay);
+   digitalWrite(BUZZER_PIN,LOW);
+   delayMicroseconds(uDelay);
+ }
 }
