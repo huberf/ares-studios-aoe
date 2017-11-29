cb205a0bd5146ef9275bb17d9f5d40a2a6e1c419
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-11-29 18:09:28 -0500

    Add new phaser schematics and shift register support to phaser

--- phaser-control/phaser-control.ino
+++ phaser-control/phaser-control.ino
@@ -8,6 +8,12 @@ const int TRIGGER_BUTTON = 5;
 const int LASER_PIN = 6;
 const int BUZZER_PIN = 9;
 
+const int DATA_PIN = 2;
+const int CLOCK_PIN = 3;
+const int LATCH_PIN = 4;
+
+byte data = 0;
+
 // States
 boolean isFiring = false;
 boolean buttonPressedLastLoop = false;
@@ -26,6 +32,10 @@ void setup() {
   pinMode(TRIGGER_BUTTON, INPUT);
   pinMode(LASER_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
+  // Setup shift register
+  pinMode(DATA_PIN, OUTPUT);
+  pinMode(CLOCK_PIN, OUTPUT);  
+  pinMode(LATCH_PIN, OUTPUT);
 }
 
 void loop() {
@@ -47,6 +57,7 @@ void loop() {
     cyclesWithButtonPressed = 0;
     buttonPressedLastLoop = false;
   }
+  updateDisplay();
   delay(10);
 }
 
@@ -141,6 +152,14 @@ void alertLowAmmo() {
 
 void updateDisplay() {
   // TODO: Build logic to update LCD with shots left
+  int maxLeds = 8;
+  int litLeds = shotsLeft/2;
+  for (int i = 0; i < litLeds; i++) {
+    shiftWrite(i, true);
+  }
+  for (int i = (litLeds - 1); i < maxLeds; i++) {
+    shiftWrite(i, false);
+  }
 }
 
 void buzzPiezzo(int frequency, int duration) {
@@ -156,4 +175,54 @@ void soundFX(float amplitude,float period){
    digitalWrite(BUZZER_PIN,LOW);
    delayMicroseconds(uDelay);
  }
+}
+
+/**
+ * Shift Register Lighting
+ * From Adafruit shift register tutorial
+ **/
+void shiftWrite(int desiredPin, boolean desiredState)
+
+// This function lets you make the shift register outputs
+// HIGH or LOW in exactly the same way that you use digitalWrite().
+
+// Like digitalWrite(), this function takes two parameters:
+
+//    "desiredPin" is the shift register output pin
+//    you want to affect (0-7)
+
+//    "desiredState" is whether you want that output
+//    to be HIGH or LOW
+
+// Inside the Arduino, numbers are stored as arrays of "bits",
+// each of which is a single 1 or 0 value. Because a "byte" type
+// is also eight bits, we'll use a byte (which we named "data"
+// at the top of this sketch) to send data to the shift register.
+// If a bit in the byte is "1", the output will be HIGH. If the bit
+// is "0", the output will be LOW.
+
+// To turn the individual bits in "data" on and off, we'll use
+// a new Arduino commands called bitWrite(), which can make
+// individual bits in a number 1 or 0.
+{
+  // First we'll alter the global variable "data", changing the
+  // desired bit to 1 or 0:
+
+  bitWrite(data,desiredPin,desiredState);
+
+  // Now we'll actually send that data to the shift register.
+  // The shiftOut() function does all the hard work of
+  // manipulating the data and clock pins to move the data
+  // into the shift register:
+
+  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, data);
+
+  // Once the data is in the shift register, we still need to
+  // make it appear at the outputs. We'll toggle the state of
+  // the latchPin, which will signal the shift register to "latch"
+  // the data to the outputs. (Latch activates on the low-to
+  // -high transition).
+
+  digitalWrite(LATCH_PIN, HIGH);
+  digitalWrite(LATCH_PIN, LOW);
 }
