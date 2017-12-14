bc1c1231a0705a3ef0f7cb3da1340c47015c4934
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-12-13 19:31:59 -0500

    Prevent multiple reloads when trigger is continually pressed

--- phaser-control/phaser-control.ino
+++ phaser-control/phaser-control.ino
@@ -50,7 +50,7 @@ void loop() {
     }
     buttonPressedLastLoop = true;
     cyclesWithButtonPressed += 1;
-    if (cyclesWithButtonPressed > RECHARGE_DURATION) {
+    if (cyclesWithButtonPressed > RECHARGE_DURATION && gunFull() == false) {
       reload();
       cyclesWithButtonPressed = 0;
     }
@@ -206,58 +206,10 @@ void writeLeds(int totalLit) {
   digitalWrite(LATCH_PIN, HIGH);
 }
 
-/**
- * Shift Register Lighting
- * From Adafruit shift register tutorial
- **/
-void shiftWrite(int desiredPin, boolean desiredState)
-
-// This function lets you make the shift register outputs
-// HIGH or LOW in exactly the same way that you use digitalWrite().
-
-// Like digitalWrite(), this function takes two parameters:
-
-//    "desiredPin" is the shift register output pin
-//    you want to affect (0-7)
-
-//    "desiredState" is whether you want that output
-//    to be HIGH or LOW
-
-// Inside the Arduino, numbers are stored as arrays of "bits",
-// each of which is a single 1 or 0 value. Because a "byte" type
-// is also eight bits, we'll use a byte (which we named "data"
-// at the top of this sketch) to send data to the shift register.
-// If a bit in the byte is "1", the output will be HIGH. If the bit
-// is "0", the output will be LOW.
-
-// To turn the individual bits in "data" on and off, we'll use
-// a new Arduino commands called bitWrite(), which can make
-// individual bits in a number 1 or 0.
-{
-  // First we'll alter the global variable "data", changing the
-  // desired bit to 1 or 0:
-
-  bitWrite(data,desiredPin,desiredState);
-
-  // Now we'll actually send that data to the shift register.
-  // The shiftOut() function does all the hard work of
-  // manipulating the data and clock pins to move the data
-  // into the shift register:
-
-  byte leds = 0;
-  for (int i = 0; i< desiredPin; i++) {
-    bitSet(leds, i);
+boolean gunFull() {
+  if (shotsLeft == SHOTS_CAPACITY) {
+    return true;
+  } else {
+    return false;
   }
-  //bitSet(leds, desiredPin);
-  digitalWrite(LATCH_PIN, LOW);
-  shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, data);
-
-  // Once the data is in the shift register, we still need to
-  // make it appear at the outputs. We'll toggle the state of
-  // the latchPin, which will signal the shift register to "latch"
-  // the data to the outputs. (Latch activates on the low-to
-  // -high transition).
-
-  digitalWrite(LATCH_PIN, HIGH);
-  digitalWrite(LATCH_PIN, LOW);
 }
